2080c048080386fe3262a0d7467b4381c391d6d6
ErinaMikami <erina.m29@gmail.com>
2011-07-18 18:40:04 +0900

    gitignoreファイルを設定。

---
+++ common/sample_le_config.php
@@ -0,0 +1,18 @@
+<?php
+
+//MYSQLのホスト名
+$host = "hostname";
+
+//MYSQLのユーザー名
+$user = "username";
+
+//MYSQLのパスワード
+$pass = "password";
+
+//データベース名
+$db = "database";
+
+//テーブルのキャラクターセット
+$charset = "utf8";
+
+?>
